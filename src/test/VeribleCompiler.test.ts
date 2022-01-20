bafd4790951ab9b82c77c9c36b7cb33d0c3f9306
Joe Crop <joe.crop@gmail.com>
2022-01-19 19:59:08 -0800

    more logging

--- src/test/VeribleCompiler.test.ts
+++ src/test/VeribleCompiler.test.ts
@@ -25,7 +25,7 @@ suite('VeribleCompiler Tests', () => {
         const document: TextDocument = castTextDocument(documentWorkspace);
 
         const compiledFilePath = getPathFromUri(document.uri, __dirname);
-
+        console.log("compiledFilePath: " + compiledFilePath)
         const stderrFile = path.join(__dirname, testFolderLocation, 'foo.stdout.txt');
 
         let stdout = fs.readFileSync(stderrFile).toString();
