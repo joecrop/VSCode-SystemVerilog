837192c91406d110544c71a2f6ad852c7b624ffc
Joe Crop <joe.crop@gmail.com>
2022-01-19 23:08:41 -0800

    recorrecting path

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -29,6 +29,10 @@ export function getPathFromUri(uri: string, rootPath: string): string {
     console.log("lRootPath: " + lRootPath)
     const regex = new RegExp(`/?${lRootPath}(.*)`);
     if ((matches = regex.exec(parsedUriPlatform)) != null && matches.length > 1) {
+        if (os.platform() == 'win32') {
+            let path = lRootPath + matches[1];
+            return path.replace(/\//g, '\\');
+        }
         return lRootPath + matches[1];
     }
 
