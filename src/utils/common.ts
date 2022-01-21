6a4fe5f8d9bd296d625e51fbd3a5e8baae4a7d79
Joe Crop <joe.crop@gmail.com>
2022-01-20 19:41:22 -0800

    removed loggind messages

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,5 @@
 import * as uriJs from 'uri-js';
-const os = require('os');
+import os = require('os');
 /**
     Get path from a given `uri`
 
@@ -25,8 +25,6 @@ export function getPathFromUri(uri: string, rootPath: string): string {
 
     let matches;
     const lRootPath = rootPath.replace(/\\/g, '/');
-    console.log("parsedUri.path: " + parsedUriPlatform)
-    console.log("lRootPath: " + lRootPath)
     const regex = new RegExp(`/?${lRootPath}(.*)`);
     if ((matches = regex.exec(parsedUriPlatform)) != null && matches.length > 1) {
         if (os.platform() == 'win32') {
